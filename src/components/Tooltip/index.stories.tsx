6e267fe2a5d67c3d2d3f86b7e6de617aafcebda9
okkun <okuda4645@gmail.com>
2023-09-03 13:06:38 +0900

    add floating-ui/react

---
+++ src/components/Tooltip/index.stories.tsx
@@ -0,0 +1,15 @@
+import Tooltip from "./index";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "components/Tooltip",
+  component: Tooltip,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Tooltip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,5 @@
+const Tooltip = () => {
+  return <p>Tooltip</p>;
+};
+
+export default Tooltip;
