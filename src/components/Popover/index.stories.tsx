c73fbcf35902dfe2eb7fef08a95a49464ab97244
okkun <okuda4645@gmail.com>
2023-09-09 16:24:15 +0900

    add Popover story

--- src/components/Popover/index.stories.tsx
+++ src/components/Popover/index.stories.tsx
@@ -1,5 +1,5 @@
 import Popover from "./index";
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import PopoverTrigger from "./PopoverTrigger";
 import PopoverContent from "./PopoverContent";
 import { PopoverHeading } from "./PopoverContent/PopoverHeading";
@@ -15,31 +15,28 @@ const meta = {
 } satisfies Meta<typeof Popover>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    children: (
-      <>
-        <PopoverTrigger>My trigger</PopoverTrigger>
-        <PopoverContent
-          style={{
-            backgroundColor: "white",
-            border: "1px solid #ddd",
-            borderRadius: "4px",
-            boxSizing: "border-box",
-            fontSize: "90%",
-            padding: "4px 8px",
-            boxShadow: "0 2px 4px rgba(0, 10, 20, 0.1)",
-            width: "max-content",
-            maxWidth: "calc(100vw - 10px)",
-          }}
-        >
-          <PopoverHeading>My popover heading</PopoverHeading>
-          <PopoverDescription>My popover description</PopoverDescription>
-          <PopoverClose>Close</PopoverClose>
-        </PopoverContent>
-      </>
-    ),
-  },
+export const Default = {
+  render: () => (
+    <Popover>
+      <PopoverTrigger>My trigger</PopoverTrigger>
+      <PopoverContent
+        style={{
+          backgroundColor: "white",
+          border: "1px solid #ddd",
+          borderRadius: "4px",
+          boxSizing: "border-box",
+          fontSize: "90%",
+          padding: "4px 8px",
+          boxShadow: "0 2px 4px rgba(0, 10, 20, 0.1)",
+          width: "max-content",
+          maxWidth: "calc(100vw - 10px)",
+        }}
+      >
+        <PopoverHeading>My popover heading</PopoverHeading>
+        <PopoverDescription>My popover description</PopoverDescription>
+        <PopoverClose>Close</PopoverClose>
+      </PopoverContent>
+    </Popover>
+  ),
 };
